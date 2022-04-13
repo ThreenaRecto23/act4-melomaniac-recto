7f204785c82bb0f58b96466b7558d1b7289e738e
RectoThreena <rectothreena@gmail.com>
2022-04-13 16:01:16 +0800

    song6 added

---
+++ song6.php
@@ -0,0 +1,134 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Melomaniac Activity</title>
+        <link rel="icon" type="image/x-icon" href="assets/beat.png" />
+      
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-info fixed-top" id="mainNav">
+            <div class="container px-4">
+                <a class="navbar-brand text-black" href="index.php"> BEATify </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item"><a class="nav-link text-black" href="song1.php">Song 1</a></li>
+                        <li class="nav-item"><a class="nav-link text-black" href="song2.php">Song 2</a></li>
+                        <li class="nav-item"><a class="nav-link text-black" href="song3.php">Song 3</a></li>
+                         <li class="nav-item"><a class="nav-link text-black" href="song4.php">Song 4</a></li>
+                         <li class="nav-item"><a class="nav-link text-black" href="song5.php">Song 5</a></li>
+                         <li class="nav-item"><a class="nav-link text-black" href="song6.php">Song 6</a></li>
+                         <li class="nav-item"><a class="nav-link text-black" href="song7.php">Song 7</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <!-- Header-->
+        <header class="bg-dark text-white">
+            <div class="container px-4 text-center">
+                <h1 class="fw-bolder">BEATify</h1> <br>
+                <p class="lead">"Music is to the soul what words are to the mind" --Modest Mouse </p>
+
+
+            </div>
+        </header>
+        <!-- About section-->
+        <section id="songs">
+            <style type="text/css"> body{background: linear-gradient(to right, #5D6D7E , #F4D03F  );
+             min-height: 100vh;
+             overflow-x: hidden
+            </style>
+
+            <div class="container px-4">
+                <div class="row gx-4 justify-content-center">
+                    <div class="col-lg-12 text-center">
+
+                        
+                        <?php
+
+                        echo "<h2 class='text-black'> Love Grows</h2>";
+                        echo "<p class='text-black'><b>Edison Lighthouse</p></b>";
+                        echo "<br/>";
+                
+                        echo "<p> <i><b>[G]</i></b>She ain’t got no money </p>";
+                        echo "<p> Her clothes are kinda funny</p>";
+                        echo "<p> <i><b>[C]</i></b>Her hair is kinda wild and free </p>";
+                        echo "<p> Oh, but <i><b>[G]</i></b>love <i><b>[Bm]</i></b>grows where <i><b>[Em]</i></b>my rosemary <i><b>[G]</i>
+                        </b>goes</p>";
+                        echo "<p> And <i><b>[C]</i></b>nobody <i><b>[Am]</i></b>knows like <i><b>[D7]</i></b>me </p>";
+                        echo "<br/>";
+
+                        echo "<p> <i><b>[G]</i></b>She talks kinda lazy</p>";
+                        echo "<p> People say she’s crazy</p>";
+                        echo "<p> <i><b>[C]</i></b>And her life’s a mystery</p>";
+                        echo "<p> Oh, but <i><b>[G]</i></b>love <i><b>[Bm]</i></b>grows where <i><b>[Em]</i></b>my rosemary <i><b>[G]</i>
+                        </b>goes</p>";
+                        echo "<p> And <i><b>[C]</i></b>nobody <i><b>[Am]</i></b>knows --- <i><b>[D7]</i></b>like <i><b>[G]</i></b>me</p>";
+                        echo "<br/>";
+
+                        echo "<p> <i><b>[C]</i></b>Something about her <i><b>[D]</i></b>hand holding <i><b>[G]</i></b>mine</p>";
+                        echo "<p> It’s a feeling that’s <i><b>[C]</i></b>fine</p>";
+                        echo "<p> And I <i><b>[D]</i></b>just gotta <i><b>[G]</i></b>say – <i><b>[G7]</i></b>Hey!</p>";
+                        echo "<p> She’s <i><b>[C]</i></b>really gotta <i><b>[D]</i></b>magical <i><b>[G]</i></b>spell </p>";
+                        echo "<p> And its working so <i><b>[A7]</i></b>well</p>";
+                        echo "<p> And I can’t get <i><b>[D7]</i></b>away</p>";
+                        echo "<br/>";
+
+                        echo "<p> <i><b>[G]</i></b>I’m a lucky fella </p>";
+                        echo "<p> And I just gotta tell her </p>";
+                        echo "<p> <i><b>[C]</i></b>That I love her endlessly</p>";
+                        echo "<p> because <i><b>[G]</i></b>love <i><b>[Bm]</i></b>grows where <i><b>[Em]</i></b>my rosemary <i><b>[G]</i>
+                        </b>goes</p>";
+                        echo "<p> And <i><b>[C]</i></b>nobody <i><b>[Am]</i></b>knows --- <i><b>[D7]</i></b>like <i><b>[G]</i></b>me</p>";
+                        echo "<br/>";
+
+                        echo "<p><i><b>[A] [Bm] [Em] [G] [C] [Am] [D7] [A]]</i></b>  </p>";
+                        echo "<br/>";
+
+                        echo "<p> <i><b>[C]</i></b>Something about her <i><b>[D]</i></b>hand holding <i><b>[G]</i></b>mine</p>";
+                        echo "<p> It’s a feeling that’s<i><b>[C]</i></b>fine</p>";
+                        echo "<p> And I <i><b>[D]</i></b>just gotta <i><b>[G]</i></b>say – <i><b>[G7]</i></b>Hey!</p>";
+                        echo "<p> She’s <i><b>[C]</i></b>really gotta <i><b>[D]</i></b>magical <i><b>[G]</i></b>spell </p>";
+                        echo "<p> And its working so <i><b>[A7]</i></b>well</p>";
+                        echo "<p> And I can’t get <i><b>[D7]</i></b>away <i><b>[D#7]</i></b>---</p>";
+                        echo "<br/>";
+
+                        echo "<p> <i><b>[G]</i></b>I’m a lucky fella </p>";
+                        echo "<p> And I just gotta tell her </p>";
+                        echo "<p> <i><b>[C]</i></b>That I love her endlessly</p>";
+                        echo "<p> because <i><b>[G]</i></b>love <i><b>[Bm]</i></b>grows where <i><b>[Em]</i></b>my rosemary <i><b>[G]</i>
+                        </b>goes</p>";
+                        echo "<p> And <i><b>[C]</i></b>nobody <i><b>[Am]</i></b>knows --- <i><b>[D7]</i></b>like <i><b>[G]</i></b>me</p>";
+                        echo "<br/>";
+
+                        echo "<p> l<i><b>[Ab]</i></b>ove g<i><b>[Cm]</i></b>rows where my r<i><b>[Fm]</i></b>osemary g<i><b>[Ab]</i></b>oes </p>";
+                        echo "<p> And <i><b>[Db]</i></b>nobody <i><b>[A#m]</i></b>knows <i><b>[D#]</i></b>like <i><b>[Ab]</i></b>me </p>";
+                        echo "<p> <i><b>[Ab]</i></b>love <i><b>[Cm]</i></b>grows where my <i><b>[Fm]</i></b>rosemary <i><b>[Ab]</i></b>goes </p>";
+                        echo "<p> And <i><b>[Db]</i></b>nobody <i><b>[A#m]</i></b>knows --- <i><b>[D#][Ab]</i></b>like me </p>";
+                        echo "<br/>";
+
+
+
+                        ?>
+
+
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <footer class="py-4 bg-dark">
+            <div class="container px-4"><p class="m-0 text-center text-white">Copyright &copy; 2022. Threena R. Recto</p></div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
