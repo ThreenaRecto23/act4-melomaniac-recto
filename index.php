8bc9d9da5b788b2400a5cf308897a4122bd1fcff
RectoThreena <rectothreena@gmail.com>
2022-04-13 14:55:59 +0800

    index

---
+++ index.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Melomaniac Activity</title>
+        <link rel="icon" type="image/x-icon" href="assets/beat.png" />
+      
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-info fixed-top" id="mainNav">
+            <div class="container px-4">
+                <a class="navbar-brand text-black" href="index.php"> BEATify </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item"><a class="nav-link text-black" href="#">Song 1</a></li>
+                        <li class="nav-item"><a class="nav-link text-black" href="#">Song 2</a></li>
+                        <li class="nav-item"><a class="nav-link text-black" href="#">Song 3</a></li>
+                        <li class="nav-item"><a class="nav-link text-black" href="#">Song 4</a></li>
+                        <li class="nav-item"><a class="nav-link text-black" href="#">Song 5</a></li>
+                        <li class="nav-item"><a class="nav-link text-black" href="#">Song 6</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <!-- Header-->
+        <header class="bg-dark text-white">
+            <div class="container px-4 text-center">
+                <h1 class="fw-bolder">Welcome to BEATify</h1>
+                <p class="lead">Melancholy runs through music like a message through a stick of rock. </p> <br>
+                <p class="lead">This website is best for persons with great enthusiasm for music. We hope you will gets lots of enjoyment through this. </p>
+                
+            </div>
+        </header>
+        <!-- About section-->
+        <section id="songs">
+            <style type="text/css"> body{background: linear-gradient(to right, #43e975, #43a7e9);
+             min-height: 100vh;
+             overflow-x: hidden
+            </style>
+
+            <div class="container px-4 ">
+                <div class="row gx-4 justify-content-center">
+                    <div class="col-lg-12 text-center">
+                        <h2 class="text-black">Song Titles here:</h2> <br>
+                        <p class="font-arial">Song 1: I Like You So Much, You'll Know It by: Ysabelle Cuevas.</p>
+                        <p> Please <a href='#'> click here </a> to proceed to the lyrics and guitar chords.</p> <br>
+                        <p class="font-arial">Song 2: Lemon Tree: by Fools Garden.</p>
+                        <p>Please <a href='#'> click here </a> to proceed to the lyrics and guitar chords.</p> <br>
+                        <p class="font-arial">Song 3: She Will Be Loved: by Maroon 5.</p>
+                        <p>Please <a href='#'> click here </a> to proceed to the lyrics and guitar chords.</p> <br>
+                        <p class="font-arial">Song 4: LEVITATING: by Dua Lipa.</p>
+                        <p>Please <a href='#'> click here </a> to proceed to the lyrics and guitar chords.</p> <br>
+                        <p class="font-arial">Song 5: Just The Two Of Us: by Bill Withers.</p>
+                        <p>Please <a href='#'> click here </a> to proceed to the lyrics and guitar chords.</p> <br>
+                        <p class="font-arial">Song 6: Love Grows: Edison Lighthouse.</p>
+                        <p>Please <a href='#'> click here </a> to proceed to the lyrics and guitar chords.</p> <br>
+                        <p class="font-arial">Song 7: We Are: by Hiroshi Kitadani.</p>
+                        <p>Please <a href='#'> click here </a> to proceed to the lyrics and guitar chords.</p> <br>
+
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <footer class="py-3 bg-dark">
+            <div class="container px-4"><p class="m-0 text-center text-white">Copyright &copy; 2022. Threena R. Recto</p></div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
